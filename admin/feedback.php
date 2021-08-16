b7101098ad1e8b403ad5743962602e94c5686933
Shashwat3057 <shreya3057@gmail.com>
2021-08-17 00:28:14 +0530

    LMS

---
+++ admin/feedback.php
@@ -0,0 +1,93 @@
+<?php
+  include "navbar.php";
+  include "connection.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Feedback</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+    <style type="text/css">
+    	body
+    	{
+    		background-image: url("image3.jpg");
+    	}
+    	.wrapper
+    	{
+    		padding: 10px;
+    		margin: -7px auto;
+    		width:800px;
+    		height: 500px;
+    		background-color: black;
+    		opacity: .8;
+    		color: white;
+    	}
+    	.form-control
+    	{
+    		height: 70px;
+    		width: 60%;
+    	}
+    	.scroll
+    	{
+    		width: 100%;
+    		height: 300px;
+    		overflow: auto;
+    	}
+
+    </style>
+</head>
+<body>
+	<div class="wrapper">
+		<h4>If you have any suggesions then comment below.</h4>
+		<form style ="color:brown" action="" method="post">
+			<input class="form-control" type="text" name="comment" placeholder="Write something..."><br>	
+			<input class="btn btn-default" type="submit" name="submit" value="Comment" style="width: 100px; height: 35px;">		
+		</form>
+	
+<br><br>
+	<div class="scroll">
+		<?php
+			if(isset($_POST['submit']))
+			{
+				$sql="INSERT INTO `comment` VALUES('','$_POST[comment]');";
+				if(mysqli_query($db,$sql))
+				{
+					$q="SELECT * FROM `comment` ORDER BY `comment`.`id` DESC";
+					$res=mysqli_query($db,$q);
+
+					echo "<table class='table table-bordered'>";
+					while ($row=mysqli_fetch_assoc($res)) 
+					{
+						echo "<tr>";
+							echo "<td>"; echo $row['comment']; echo "</td>";
+						echo "</tr>";
+					}
+				}
+
+			}
+
+			else
+			{
+				$q="SELECT * FROM `comment` ORDER BY `comment`.`id` DESC"; 
+					$res=mysqli_query($db,$q);
+
+					echo "<table class='table table-bordered'>";
+					while ($row =mysqli_fetch_assoc($res)) 
+					{
+						echo "<tr>";
+							echo "<td>"; echo $row['comment']; echo "</td>";
+						echo "</tr>";
+					}
+			}
+		?>
+	</div>
+	</div>
+</body>
+</html>
